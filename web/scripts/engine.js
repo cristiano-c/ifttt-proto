f5546790a6a44d94a3791214dad1d51f14771395
sablaireau <costa1.hope@libero.it>
2016-07-28 21:34:11 +0200

    date online

--- web/scripts/engine.js
+++ web/scripts/engine.js
@@ -1339,30 +1339,107 @@ iftttApp.controller('Trigger2GcalendarController', ['$scope', '$rootScope', '$ro
 
 
 iftttApp.controller('action1GcalendarController', ['$scope', '$rootScope', '$routeParams', '$http', '$location',
-    function ($scope, $rootscope, $routeParams, $http, $resource, $location)
-    {
+    function ($scope, $rootscope, $routeParams, $http, $resource, $location) {
 
-        $scope.data =
+        $scope.actiongcalendar = function()
+        {
+
+        }
+
+
+
+
+
+        $scope.yearVector =
         {
             availableOptions:
                 [
-                    {id: '1', name: 'January'},
-                    {id: '2', name: 'February'},
-                    {id: '3', name: 'March'},
-                    {id: '4', name: 'April'},
-                    {id: '5', name: 'May'},
-                    {id: '6', name: 'June'},
-                    {id: '7', name: 'July'},
-                    {id: '8', name: 'August'},
-                    {id: '9', name: 'September'},
-                    {id: '10', name: 'October'},
-                    {id: '11', name: 'November'},
-                    {id: '12', name: 'December'},
+                    {id: '1', year: '2016'},
+                    {id: '2', year: '2017'}
 
                 ],
-            selectedOption: {id: '1', name: 'January'} //This sets the default value of the select in the ui
+            selectedOption: {id: '1', year: '2016'}
 
-        }
+        };
+
+
+
+
+
+        $scope.monthVector =
+        {
+            availableOptions:
+                [
+                    {id: '1', month: 'January'},
+                    {id: '2', month: 'February'},
+                    {id: '3', month: 'March'},
+                    {id: '4', month: 'April'},
+                    {id: '5', month: 'May'},
+                    {id: '6', month: 'June'},
+                    {id: '7', month: 'July'},
+                    {id: '8', month: 'August'},
+                    {id: '9', month: 'September'},
+                    {id: '10', month: 'October'},
+                    {id: '11', month: 'November'},
+                    {id: '12', month: 'December'}
+                ],
+            selectedOption: {id: '1', month: 'January'} //This sets the default value of the select in the ui
+
+        };
+
+
+
+
+
+
+
+        $scope.dayVector =
+        {
+            availableOptions:
+                [
+                    {id: '1', day: '1°'},
+                    {id: '2', day: '2°'},
+                    {id: '3', day: '3°'},
+                    {id: '4', day: '4°'},
+                    {id: '5', day: '5°'},
+                    {id: '6', day: '6°'},
+                    {id: '7', day: '7°'},
+                    {id: '8', day: '8°'},
+                    {id: '9', day: '9°'},
+                    {id: '10', day: '10°'},
+                    {id: '11', day: '11°'},
+                    {id: '12', day: '12°'},
+                    {id: '13', day: '13°'},
+                    {id: '14', day: '14°'},
+                    {id: '15', day: '15°'},
+                    {id: '16', day: '16°'},
+                    {id: '17', day: '17°'},
+                    {id: '18', day: '18°'},
+                    {id: '19', day: '19°'},
+                    {id: '20', day: '20°'},
+                    {id: '21', day: '21°'},
+                    {id: '22', day: '22°'},
+                    {id: '23', day: '23°'},
+                    {id: '24', day: '24°'},
+                    {id: '25', day: '25°'},
+                    {id: '26', day: '26°'},
+                    {id: '27', day: '27°'},
+                    {id: '28', day: '28°'},
+                    {id: '29', day: '29°'},
+                    {id: '30', day: '30°'},
+                    {id: '31', day: '31°'}
+                ],
+            selectedOption: {id: '1', day: '1°'}
+
+        };
+
+
+
+
+
+
 //action1GcalendarController
 
     }]);
+
+
